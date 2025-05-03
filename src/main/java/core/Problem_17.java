d969843eeba4898fe7930f0ae6362d1963208eec
chandrasekharkillampalli <chandrasekhar.killampalli@gmail.com>
2025-05-03 11:49:51 +0530

    Get the list of names starting with first letter

---
+++ src/main/java/core/Problem_17.java
@@ -0,0 +1,29 @@
+package core;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Problem_17 {
+
+    public static void main(String...args) {
+        String[] strArray = {"raj", "rahul", "ankita", "arpita", "priya"};
+
+        //output : {p=[priya], a=[ankita, arpita], r=[raj, rahul]}
+
+        Map<String, List<String>> result = Arrays.stream(strArray)
+                .collect(Collectors.groupingBy(
+                        s -> String.valueOf(s.charAt(0)),
+                        TreeMap::new, // To keep keys sorted
+                        Collectors.toList()
+                ));
+
+        System.out.println(result);
+    }
+
+}
